32163d8e7964fa9f0bb8c1f6c6be0306775df866
MIKE <kpocobok.muxa@mail.ru>
2023-12-15 05:25:22 +0300

    добавление 3 задачи

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int InputNumber(string text) 
+{ 
+    do 
+    { 
+        Console.Write(text); 
+        if (int.TryParse(Console.ReadLine(), out int index) && index >= 0) 
+ 
+            return index; 
+ 
+        else 
+             Console.WriteLine("Некорректный ввод. Введите положительное целое число.");  
+ 
+    } while (true); 
+} 
+ 
+int[] GenerateArray(int size, int rightRange, int leftRange) 
+{ 
+    Random rand = new Random(); 
+    int[] newArray = new int[size]; 
+ 
+    for (int i = 0; i < size; i++) 
+    { 
+        newArray[i] = rand.Next(rightRange, leftRange-1); 
+    } 
+ 
+    return newArray; 
+} 
+ 
+void PrintReverseArray(int[] array, int index) 
+{ 
+    if (index >= 0) 
+    { 
+        Console.Write(array[index] + " "); 
+        PrintReverseArray(array, index - 1); 
+    } 
+} 
+ 
+int size = InputNumber("Введите размер массива: "); 
+int[] myArray = GenerateArray(size, 1, 20); 
+ 
+Console.Write($"[{string.Join(", ", myArray)}] => "); 
+PrintReverseArray(myArray, size - 1); 
+Console.WriteLine();
