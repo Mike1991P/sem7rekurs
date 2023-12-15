2d0da05ec5e9e14ad80c8473dca498c6bb5918e1
MIKE <kpocobok.muxa@mail.ru>
2023-12-15 05:23:09 +0300

    решение второй задачи

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите значение M:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int akkerman(int m, int n)
+{
+if (m == 0) return n + 1;
+else if (n == 0) return akkerman(m - 1, 1);
+else return akkerman(m - 1, akkerman(m, n - 1));
+}
+
+Console.Write($"Функция Аккермана равна {akkerman(m, n)} ");
